a5d75de50f7f64be5cb6fe114fb54872f4f574b6
Ashwani <ashwani.profession@gmail.com>
2024-10-01 23:05:11 +0530

    first commit

---
+++ 1.Hello World.go
@@ -0,0 +1,13 @@
+// SITE : https://gobyexample.com/
+// GITHUB : https://github.com/mmcgrana/gobyexample
+
+
+// Our first program will print the classic "hello world"
+// message. Here's the full source code.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
